a55b0c66ed990fac6e2395bd08ba54cfe51e547b
mineAC30 <discuss.yacht.intermit.filial@gmail.com>
2020-10-09 22:56:17 +0900

    for test

--- tools/carcontrols/debug_rcv_joystick.py
+++ tools/carcontrols/debug_rcv_joystick.py
@@ -24,35 +24,24 @@ def steer_thread():
   button_1_last = 0
   enabled = False
 
-  # wait for health and CAN packets
-  hw_type = messaging.recv_one(health).health.hwType
-  has_relay = hw_type in [HwType.blackPanda, HwType.uno, HwType.dos]
-  print("Waiting for CAN messages...")
-  get_one_can(logcan)
-  # if printting is posible
-  #print(logcan)
-  print("CI/CP get_car")
-  CI, CP = get_car(logcan, sendcan, has_relay)
-  Params().put("CarParams", CP.to_bytes())
-  print("car.control.new_message")
-  CC = car.CarControl.new_message()
+
 
   print("-------------------------------------")
   print("main loop running")
   print("-------------------------------------")
 
   while True:
-
+    #print("send")
     # send
     joystick = messaging.recv_one(joystick_sock)
-    can_strs = messaging.drain_sock_raw(logcan, wait_for_one=True)
-    CS = CI.update(CC, can_strs)
-
+    #can_strs = messaging.drain_sock_raw(logcan, wait_for_one=True)
+    #print("new.messaage")
     # Usually axis run in pairs, up/down for one, and left/right for
     # the other.
     actuators = car.CarControl.Actuators.new_message()
 
     if joystick is not None:
+      #print("joystick get")
       axis_3 = clip(-joystick.testJoystick.axes[3] * 1.05, -1., 1.)          # -1 to 1
       actuators.steer = axis_3
       actuators.steerAngle = axis_3 * 43.   # deg
@@ -67,7 +56,6 @@ def steer_thread():
       pcm_cancel_cmd = joystick.testJoystick.buttons[0]
       button_1 = joystick.testJoystick.buttons[1]
       if button_1 and not button_1_last:
-
         enabled = not enabled
 
       button_1_last = button_1
@@ -79,26 +67,26 @@ def steer_thread():
         print("hud_alert")
         hud_alert = "steerRequired"
 
-    CC.actuators.gas = actuators.gas
-    CC.actuators.brake = actuators.brake
-    CC.actuators.steer = actuators.steer
-    CC.actuators.steerAngle = actuators.steerAngle
-    CC.hudControl.visualAlert = hud_alert
-    CC.hudControl.setSpeed = 20
-    CC.cruiseControl.cancel = pcm_cancel_cmd
-    CC.enabled = enabled
-    can_sends = CI.apply(CC)
-    sendcan.send(can_list_to_can_capnp(can_sends, msgtype='sendcan'))
+    #CC.actuators.gas = actuators.gas
+    # CC.actuators.brake = actuators.brake
+    # CC.actuators.steer = actuators.steer
+    # CC.actuators.steerAngle = actuators.steerAngle
+    # CC.hudControl.visualAlert = hud_alert
+    # CC.hudControl.setSpeed = 20
+    # CC.cruiseControl.cancel = pcm_cancel_cmd
+    # CC.enabled = enabled
+    # can_sends = CI.apply(CC)
+    #sendcan.send(can_list_to_can_capnp(can_sends, msgtype='sendcan'))
 
     # broadcast carState
-    cs_send = messaging.new_message('carState')
-    cs_send.carState = copy(CS)
-    carstate.send(cs_send.to_bytes())
+    # cs_send = messaging.new_message('carState')
+    # cs_send.carState = copy(CS)
+    # carstate.send(cs_send.to_bytes())
 
     # broadcast carControl
-    cc_send = messaging.new_message('carControl')
-    cc_send.carControl = copy(CC)
-    carcontrol.send(cc_send.to_bytes())
+    # cc_send = messaging.new_message('carControl')
+    # cc_send.carControl = copy(CC)
+    # carcontrol.send(cc_send.to_bytes())
 
 
 if __name__ == "__main__":
